82689e566ee778f0f9a1f358c0bd4357d828c40c
Evgeniy Medvedev <yevgeniy.medvedev@gmail.com>
2022-05-03 15:59:16 +0600

    Adding pyproject.toml

--- zinnia/apps.py
+++ zinnia/apps.py
@@ -9,7 +9,7 @@ class ZinniaConfig(AppConfig):
     """
     name = 'zinnia'
     label = 'zinnia'
-    verbose_name = _('Weblog')
+    verbose_name = _('WS: Articles')
 
     def ready(self):
         from django_comments.moderation import moderator
